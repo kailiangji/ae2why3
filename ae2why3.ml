566d4f414b794609f71adefec5e9b61effc63c45
Kailiang Ji <kailiang.ji2013@gmail.com>
2016-08-26 16:28:28 +0200

    replace  the char '.' in theory names by '_';

--- ae2why3.ml
+++ ae2why3.ml
@@ -28,11 +28,11 @@ let name_theory ori_file =
   if !j = 0 then
     let name1 = String.sub ori_file 0 (n-4) in
     let name2 = String.uppercase name1 in
-    name2
+    String.map (fun c -> if c = '.' then '_' else c) name2
   else
     let name1 = String.sub ori_file (!j+1) (n-1-(!j)-4) in
     let name2 = String.uppercase name1 in
-    name2
+    String.map (fun c -> if c = '.' then '_' else c) name2
 
 let axioms_to_remove =
   ["Zero"; "ONearestTiesToEven";"Add";"Sub";"Mul";"Neg";
